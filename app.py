2eb94b21c48ba8436839bd51202cbf4f1bb40151
ossuchas <suchat.s14102526@gmail.com>
2019-10-25 04:47:51 +0700

    second version

--- app.py
+++ app.py
@@ -18,4 +18,5 @@ def hello_world():
 if __name__ == '__main__':
     db.init_app(app)
     ma.init_app(app)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(port=5000)
