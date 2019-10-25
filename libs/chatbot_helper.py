eba0cdf1ba4ea521d09dab02e5975cb76bdb9d14
ossuchas <suchat.s14102526@gmail.com>
2019-10-26 06:21:40 +0700

    aded#

--- libs/chatbot_helper.py
+++ libs/chatbot_helper.py
@@ -13,37 +13,37 @@ def replyMsg(Reply_token, TextMessage, line_Acees_Token):
         'Authorization': authorization
     }
 
-    # data = {
-    #     "replyToken": Reply_token,
-    #     "messages": [{
-    #         "type": "text",
-    #         "text": TextMessage
-    #     }]
-    # }
-
     data = {
         "replyToken": Reply_token,
         "messages": [{
-            "type": "template",
-            "altText": "this is a confirm template",
-            "template": {
-                "type": "confirm",
-                "actions": [
-                    {
-                        "type": "uri",
-                        "label": "Yes",
-                        "uri": "http://www.google.com"
-                    },
-                    {
-                        "type": "message",
-                        "label": "No",
-                        "text": "No"
-                    }
-                ],
-                "text": "Continue?"
-            }
+            "type": "text",
+            "text": TextMessage
         }]
     }
+
+    # data = {
+    #     "replyToken": Reply_token,
+    #     "messages": [{
+    #         "type": "template",
+    #         "altText": "this is a confirm template",
+    #         "template": {
+    #             "type": "confirm",
+    #             "actions": [
+    #                 {
+    #                     "type": "uri",
+    #                     "label": "Yes",
+    #                     "uri": "http://www.google.com"
+    #                 },
+    #                 {
+    #                     "type": "message",
+    #                     "label": "No",
+    #                     "text": "No"
+    #                 }
+    #             ],
+    #             "text": "Continue?"
+    #         }
+    #     }]
+    # }
 
     session = requests.Session()
     response = session.post(LINE_API, data=json.dumps(data), headers=headers)
