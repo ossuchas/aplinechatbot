cd642524bf7d14d58acde2c7ff0a598b288c2e13
ossuchas <suchat.s14102526@gmail.com>
2019-10-30 05:03:57 +0700

    added

--- libs/chatbot_helper.py
+++ libs/chatbot_helper.py
@@ -13,13 +13,46 @@ def replyMsg(Reply_token, TextMessage, line_Acees_Token):
         'Authorization': authorization
     }
 
+    # data = {
+    #     "replyToken": Reply_token,
+    #     "messages": [{
+    #         "type": "text",
+    #         "text": TextMessage
+    #     }]
+    # }
+
     data = {
         "replyToken": Reply_token,
         "messages": [{
-            "type": "text",
-            "text": TextMessage
+                "type": "template",
+                "altText": "this is a buttons template",
+                "template": {
+                    "type": "buttons",
+                    "actions": [
+                        {
+                            "type": "uri",
+                            "label": "go to google",
+                            "uri": "http://www.google.com"
+                        },
+                        {
+                            "type": "message",
+                            "label": "Action 2",
+                            "text": "Action 2"
+                        },
+                        {
+                            "type": "message",
+                            "label": "Action 3",
+                            "text": "Action 3"
+                        }
+                    ],
+                    "thumbnailImageUrl": "https://sirichaiwatt.com/wp-content/uploads/2018/03/SirichaiwattAtt-01-326x236.jpg",
+                    "title": "บทความดีดี สร้างแรงบันดาลใจ",
+                    "text": "by suchat_s"
+                }
         }]
     }
+
+
 
 
     session = requests.Session()
