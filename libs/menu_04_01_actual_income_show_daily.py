746083474cdfe43e5aadff4bf7b292d7b07f140b
ossuchas <suchat.s14102526@gmail.com>
2019-12-23 16:58:11 +0700

    modified card three

--- libs/menu_04_01_actual_income_show_daily.py
+++ libs/menu_04_01_actual_income_show_daily.py
@@ -186,29 +186,30 @@ def replyMsg(reply_token: str = None, text_msg: str = None,
                        "width": "32px", "height": "32px", "offsetBottom": "2px"}]},
                  "styles": {"header": {"backgroundColor": "#000000"},
                             "footer": {"backgroundColor": "#000000"}}
-                 },
-                {"type": "bubble", "size": "giga", "header":
-                    {"type": "box", "layout": "baseline", "contents":
-                        [{"type": "text", "text": "Actual income", "color": "#FFFFFF", "size": "lg", "weight": "bold"},
-                         {"type": "text", "text": "Before Yesterday",
-                          "align": "end", "size": "xs", "color": "#FFFFFF", "flex": 0, "style": "italic"}
-                         ]
-                     },
-                 "hero": {"type": "box", "layout": "baseline", "contents":
-                     [{"type": "text", "text": "Transfer Date At {}".format(date_val_b_last), "align": "end",
-                       "weight": "bold",
-                       "offsetEnd": "20px", "size": "sm", "color": "#939393"}]},
-                 # "body": {"type": "box", "layout": "vertical", "contents": before_last_new_contents},
-                 # "footer": {"type": "box", "layout": "vertical", "contents":
-                 #     [{"type": "text", "text": "Copyright 2019 AP PCL.", "size": "xs", "align": "center",
-                 #       "color": "#ffffff"},
-                 #      {"type": "box", "layout": "vertical", "contents":
-                 #          [{"type": "image", "url": "https://i.ibb.co/fS0B4wP/AP-Logo-2018.png"}],
-                 #       "position": "absolute",
-                 #       "width": "32px", "height": "32px", "offsetBottom": "2px"}]},
-                 # "styles": {"header": {"backgroundColor": "#000000"},
-                 #            "footer": {"backgroundColor": "#000000"}}
                  }
+                # ,
+                # {"type": "bubble", "size": "giga", "header":
+                #     {"type": "box", "layout": "baseline", "contents":
+                #         [{"type": "text", "text": "Actual income", "color": "#FFFFFF", "size": "lg", "weight": "bold"},
+                #          {"type": "text", "text": "Before Yesterday",
+                #           "align": "end", "size": "xs", "color": "#FFFFFF", "flex": 0, "style": "italic"}
+                #          ]
+                #      },
+                #  "hero": {"type": "box", "layout": "baseline", "contents":
+                #      [{"type": "text", "text": "Transfer Date At {}".format(date_val_b_last), "align": "end",
+                #        "weight": "bold",
+                #        "offsetEnd": "20px", "size": "sm", "color": "#939393"}]},
+                #  "body": {"type": "box", "layout": "vertical", "contents": before_last_new_contents},
+                #  "footer": {"type": "box", "layout": "vertical", "contents":
+                #      [{"type": "text", "text": "Copyright 2019 AP PCL.", "size": "xs", "align": "center",
+                #        "color": "#ffffff"},
+                #       {"type": "box", "layout": "vertical", "contents":
+                #           [{"type": "image", "url": "https://i.ibb.co/fS0B4wP/AP-Logo-2018.png"}],
+                #        "position": "absolute",
+                #        "width": "32px", "height": "32px", "offsetBottom": "2px"}]},
+                #  "styles": {"header": {"backgroundColor": "#000000"},
+                #             "footer": {"backgroundColor": "#000000"}}
+                #  }
             ]
         }
     }
