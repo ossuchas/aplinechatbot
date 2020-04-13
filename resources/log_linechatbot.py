e33e64cb553bba4e8ab82a998877e07d616f5f8c
ossuchas <suchat.s14102526@gmail.com>
2020-04-14 04:48:21 +0700

    add new api for add log walk summary by BG#4

--- resources/log_linechatbot.py
+++ resources/log_linechatbot.py
@@ -14,17 +14,17 @@ class LogLineChatBot(Resource):
 
         log_models.replyToken = '8ab9ecda51bf4eb89e3617ed0358ad3d'
         log_models.source_groupId = None
-        log_models.source_userId = 'U80a30a5bad4ea0f5f7995e5050ab8d7e'
+        log_models.source_userId = 'U2afc49c6b047dd285e8c926ac62d7f62'
         log_models.source_type = 'user'
-        log_models.timestamps = '1586810463231'
-        log_models.message_type = 'text'
+        log_models.timestamps = str(datetime.timestamp(datetime.now()))
+        log_models.message_type = 'web'
         log_models.message_text = 'Walk Summary by BG'
         log_models.stickerId = None
         log_models.packageId = None
         log_models.beacon_hwid = None
         log_models.beacon_dm = None
         log_models.beacon_type = None
-        log_models.beacon_entrydate = datetime.now()
+        log_models.beacon_entrydate = None
         log_models.register_flag = 'N'
         log_models.register_empid = None
         log_models.register_email = None
