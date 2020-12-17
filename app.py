4f0c7ed7f9b473d322c2011d752cb581f532e905
ossuchas <suchat.s14102526@gmail.com>
2020-12-17 13:17:19 +0700

    Modified get menu to db production

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ app.config.from_envvar("APPLICATION_SETTING")
 
 @app.route('/')
 def hello_world():
-    return 'CRM@APLineChat Bot version v16.0.07'
+    return 'CRM@APLineChat Bot version v16.0.08'
 
 
 api.add_resource(ChatBot, "/webhook")
