c9bcf92d3412ff420a46db219aa29c37239caae7
ossuchas <suchat.s14102526@gmail.com>
2020-12-17 07:02:55 +0700

    Modified get version line chatbot production

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ app.config.from_envvar("APPLICATION_SETTING")
 
 @app.route('/')
 def hello_world():
-    return 'CRM@APLineChat Bot version v16.0.06'
+    return 'CRM@APLineChat Bot version v16.0.07'
 
 
 api.add_resource(ChatBot, "/webhook")
